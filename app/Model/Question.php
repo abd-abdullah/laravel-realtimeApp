8fb4bd6a5265e8e16c7ee1a0cae1dfe2ada16d0d
Md. Abdullah <abdullah@systechdigital.com>
2019-09-27 16:50:30 +0600

    add relationship

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,23 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Question extends Model
 {
-    //
+
+    public function user()
+    {
+    	return $this->belongsTo('User');
+    }
+
+    public function replies()
+    {
+    	return $this->hasMany('Reply')
+    }
+
+    public function category()
+    {
+    	return $this->belongsTo('Category')
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function user()
+    {
+    	return $this->belongsTo('User');
+    }
+
+    public function question()
+    {
+    	return $this->belongsTo('Question');
+    }
+
+    public function like()
+    {
+    	return $this->hasMany('Like');
+    }
+
 }
