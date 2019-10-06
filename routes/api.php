356634d1e69784a124162f5155e92a0cc88a8a50
Md. Abdullah <abdullah@systechdigital.com>
2019-10-06 23:41:06 +0600

    added JWT package

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,14 @@ Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
 
+Route::post('like/{reply}', 'LikeController@LikeIt');
+Route::delete('like/{reply}', 'LikeController@UnLikeIt');
+
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
