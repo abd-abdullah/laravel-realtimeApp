b8eaddc8723d8c0f94d097378bd06cca76568629
Md. Abdullah <abdullah@systechdigital.com>
2019-10-08 23:29:51 +0600

    added bootstrap-vue & router

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- Footer -->
+    <div>
+        <div class="bg-dark text-warning footer-copyright text-center py-3">© {{ new Date().getFullYear() }} Copyright:
+            <a class="text-white-50" href="#">Abdullah- abdullah001rti@gmail.com</a>
+        </div>
+    </div>
+
+    <!-- Footer -->
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <AppNavbar></AppNavbar>
+        dr
+        <AppFooter></AppFooter>
+    </div>
+</template>
+
+<script>
+    import AppNavbar from './AppNavbar'
+    import AppFooter from './AppFooter'
+    export default {
+        components:{AppNavbar, AppFooter}
+    }
+</script>
+
+<style scoped>
+
+</style>
